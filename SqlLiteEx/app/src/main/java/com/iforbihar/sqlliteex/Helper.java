e9bfbeec235709ed85578e8f4c1c74a420068b1a
unknown <anorak.act@INVIND529.win2k.internal>
2021-08-05 09:31:43 +0530

    sqllite ex

--- SqlLiteEx/app/src/main/java/com/iforbihar/sqlliteex/Helper.java
+++ SqlLiteEx/app/src/main/java/com/iforbihar/sqlliteex/Helper.java
@@ -2,8 +2,10 @@ package com.iforbihar.sqlliteex;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
@@ -42,5 +44,17 @@ public class Helper extends SQLiteOpenHelper {
         }catch (Exception ex){
             return ex.getMessage();
         }
+    }
+
+    public Cursor fetchAllData() {
+        Cursor cursor=null;
+        try{
+            database=getReadableDatabase();
+            String[] columns=new String[]{name,contact};
+            cursor=database.query(tableName,columns,null,null,null,null,null);
+        }catch (Exception ex){
+            Log.i("sqlite_error",ex.getMessage());
+        }
+        return cursor;
     }
 }
--- SqlLiteEx/app/src/main/java/com/iforbihar/sqlliteex/MainActivity.java
+++ SqlLiteEx/app/src/main/java/com/iforbihar/sqlliteex/MainActivity.java
@@ -2,17 +2,23 @@ package com.iforbihar.sqlliteex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.GridView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Helper helper;
     EditText name,contact;
     TextView tv;
-    Button save;
+    Button save,fetch;
+    GridView allrecordsview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +29,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         contact=findViewById(R.id.edt_contact);
         save=findViewById(R.id.btnsave);
         save.setOnClickListener(this);
+        allrecordsview=findViewById(R.id.mylist);
+        fetch=findViewById(R.id.btnfetch);
+        fetch.setOnClickListener(this);
     }
 
     @Override
@@ -31,7 +40,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btnsave:
                 String msg=helper.onSave(name.getText().toString(),contact.getText().toString());
                 tv.setText(msg);
+                clearText();
+                break;
+            case R.id.btnfetch:
+                Cursor cursor=helper.fetchAllData();
+                loadCursorDataToGridView(cursor);
                 break;
         }
+    }
+
+    private void loadCursorDataToGridView(Cursor cursor) {
+        cursor.moveToFirst();
+        if (cursor!=null&&cursor.getColumnCount()>0){
+            ArrayList data=new ArrayList();
+            while (cursor.moveToNext()){
+                data.add(cursor.getString(0));
+                data.add(cursor.getString(1));
+            }
+            ArrayAdapter adapter=new ArrayAdapter(this, android.R.layout.simple_list_item_1,data);
+            allrecordsview.setAdapter(adapter);
+        }else{
+            tv.setText("No record available to display");
+        }
+    }
+    private void clearText(){
+        name.setText("");
+        contact.setText("");
     }
 }
